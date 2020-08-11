7378ce61b06dc3d63cb22968493f3318cbb92258
Vlad Frolov <frolvlad@gmail.com>
2020-08-11 11:53:49 +0300

    feat(client): Extended query to be able to refere to the genesis and the earliest [oldest] available blocks
    This is necessary for RosettaRPC implementation and potentially useful for NEAR Indexer.
    
    Testing Plan
    ============
    
    Added minimal RPC tests.

--- chain/jsonrpc/client/src/lib.rs
+++ chain/jsonrpc/client/src/lib.rs
@@ -9,7 +9,7 @@ use near_primitives::hash::CryptoHash;
 use near_primitives::rpc::{
     RpcGenesisRecordsRequest, RpcQueryRequest, RpcStateChangesRequest, RpcStateChangesResponse,
 };
-use near_primitives::types::{BlockId, BlockIdOrFinality, MaybeBlockId, ShardId};
+use near_primitives::types::{BlockId, BlockReference, MaybeBlockId, ShardId};
 use near_primitives::views::{
     BlockView, ChunkView, EpochValidatorInfo, FinalExecutionOutcomeView, GasPriceView,
     GenesisRecordsView, QueryResponse, StatusResponse,
@@ -216,7 +216,7 @@ impl JsonRpcClient {
         call_method(&self.client, &self.server_addr, "block", [block_id])
     }
 
-    pub fn block(&self, request: BlockIdOrFinality) -> RpcRequest<BlockView> {
+    pub fn block(&self, request: BlockReference) -> RpcRequest<BlockView> {
         call_method(&self.client, &self.server_addr, "block", request)
     }
 
